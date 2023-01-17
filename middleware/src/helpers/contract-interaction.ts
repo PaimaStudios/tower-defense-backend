fbbda5ce7dbc60e3c52d3a8a8b47442e7f49428d
Lukas Miskovsky <miskovskylukas@gmail.com>
2023-01-17 10:40:18 +0100

    Autoformat

--- middleware/src/helpers/contract-interaction.ts
+++ middleware/src/helpers/contract-interaction.ts
@@ -1,26 +1,14 @@
-import { numberToHex, utf8ToHex } from "web3-utils";
+import { numberToHex, utf8ToHex } from 'web3-utils';
 
-import { getTxTemplate } from "paima-engine/paima-tx";
-import { retrieveFee, wait } from "paima-engine/paima-utils";
-import { buildEndpointErrorFxn, CatapultMiddlewareErrorCode } from "../errors";
-import {
-    getFee,
-    getPostingMode,
-    getStorageAddress,
-    getWeb3,
-    PostingMode,
-    setFee,
-} from "../state";
-import type {
-    BatchedSubunit,
-    BatcherPostResponse,
-    BatcherTrackResponse,
-    Result,
-} from "../types";
-import { batchedToJsonString, buildBatchedSubunit } from "./data-processing";
-import { batcherEndpointCall, postToBatcher } from "./server-interaction";
-import { signMessageCardano } from "./wallet-cardano";
-import { signMessageEth } from "./wallet-metamask";
+import { getTxTemplate } from 'paima-engine/paima-tx';
+import { retrieveFee, wait } from 'paima-engine/paima-utils';
+import { buildEndpointErrorFxn, CatapultMiddlewareErrorCode } from '../errors';
+import { getFee, getPostingMode, getStorageAddress, getWeb3, PostingMode, setFee } from '../state';
+import type { BatchedSubunit, BatcherPostResponse, BatcherTrackResponse, Result } from '../types';
+import { batchedToJsonString, buildBatchedSubunit } from './data-processing';
+import { batcherEndpointCall, postToBatcher } from './server-interaction';
+import { signMessageCardano } from './wallet-cardano';
+import { signMessageEth } from './wallet-metamask';
 
 const BATCHER_WAIT_PERIOD = 500;
 const BATCHER_RETRIES = 50;
@@ -28,171 +16,134 @@ const BATCHER_RETRIES = 50;
 const TX_VERIFICATION_DELAY = 5000;
 
 export async function updateFee() {
-    try {
-        let web3 = await getWeb3();
-        const newFee = await retrieveFee(getStorageAddress(), web3);
-        setFee(newFee);
-        console.log(
-            `[updateFee] retrieved fee ${newFee}, ${newFee.length} symbols`
-        );
-    } catch (err) {
-        console.log("[updateFee] error while updating fee:", err);
-    }
+  try {
+    let web3 = await getWeb3();
+    const newFee = await retrieveFee(getStorageAddress(), web3);
+    setFee(newFee);
+    console.log(`[updateFee] retrieved fee ${newFee}, ${newFee.length} symbols`);
+  } catch (err) {
+    console.log('[updateFee] error while updating fee:', err);
+  }
 }
 
 // On success returns the block number of the transaction.
 export async function postConciselyEncodedData(
-    userAddress: string,
-    methodName: string,
-    values: string[]
+  userAddress: string,
+  methodName: string,
+  values: string[]
 ): Promise<Result<number>> {
-    const errorFxn = buildEndpointErrorFxn("postConciselyEncodedData");
-    const gameInput = values.join("|");
-    const postingMode = getPostingMode();
+  const errorFxn = buildEndpointErrorFxn('postConciselyEncodedData');
+  const gameInput = values.join('|');
+  const postingMode = getPostingMode();
 
-    switch (postingMode) {
-        case PostingMode.UNBATCHED:
-            return postString(userAddress, methodName, gameInput);
-        case PostingMode.BATCHED_ETH:
-            return buildBatchedSubunit(
-                signMessageEth,
-                userAddress,
-                gameInput
-            ).then(submitToBatcher);
-        case PostingMode.BATCHED_CARDANO:
-            return buildBatchedSubunit(
-                signMessageCardano,
-                userAddress,
-                gameInput
-            ).then(submitToBatcher);
-        default:
-            return errorFxn(
-                CatapultMiddlewareErrorCode.INTERNAL_INVALID_POSTING_MODE,
-                `Invalid posting mode: ${postingMode}`
-            );
-    }
+  switch (postingMode) {
+    case PostingMode.UNBATCHED:
+      return postString(userAddress, methodName, gameInput);
+    case PostingMode.BATCHED_ETH:
+      return buildBatchedSubunit(signMessageEth, userAddress, gameInput).then(submitToBatcher);
+    case PostingMode.BATCHED_CARDANO:
+      return buildBatchedSubunit(signMessageCardano, userAddress, gameInput).then(submitToBatcher);
+    default:
+      return errorFxn(
+        CatapultMiddlewareErrorCode.INTERNAL_INVALID_POSTING_MODE,
+        `Invalid posting mode: ${postingMode}`
+      );
+  }
 }
 
 export async function postString(
-    userAddress: string,
-    methodName: string,
-    dataUtf8: string
+  userAddress: string,
+  methodName: string,
+  dataUtf8: string
 ): Promise<Result<number>> {
-    const errorFxn = buildEndpointErrorFxn("postString");
-    const hexData = utf8ToHex(dataUtf8);
-    const txTemplate = getTxTemplate(getStorageAddress(), methodName, hexData);
-    const tx = {
-        ...txTemplate,
-        from: userAddress,
-        value: numberToHex(getFee()),
-    };
+  const errorFxn = buildEndpointErrorFxn('postString');
+  const hexData = utf8ToHex(dataUtf8);
+  const txTemplate = getTxTemplate(getStorageAddress(), methodName, hexData);
+  const tx = {
+    ...txTemplate,
+    from: userAddress,
+    value: numberToHex(getFee()),
+  };
 
-    try {
-        const txHash = await (window as any).ethereum.request({
-            method: "eth_sendTransaction",
-            params: [tx],
-        });
-        return verifyTx(txHash, TX_VERIFICATION_DELAY);
-    } catch (err) {
-        return errorFxn(
-            CatapultMiddlewareErrorCode.ERROR_POSTING_TO_CHAIN,
-            err
-        );
-    }
+  try {
+    const txHash = await (window as any).ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [tx],
+    });
+    return verifyTx(txHash, TX_VERIFICATION_DELAY);
+  } catch (err) {
+    return errorFxn(CatapultMiddlewareErrorCode.ERROR_POSTING_TO_CHAIN, err);
+  }
 }
 
 // Verifies that the transaction took place and returns its block number on success
-async function verifyTx(
-    txHash: string,
-    msTimeout: number
-): Promise<Result<number>> {
-    return new Promise((res, err) => {
-        setTimeout(async () => {
-            const web3 = await getWeb3();
-            web3.eth
-                .getTransactionReceipt(txHash)
-                .then(receipt =>
-                    res({
-                        success: true,
-                        result: receipt.blockNumber,
-                    })
-                )
-                .catch((error: any) => err(Error(error.message)));
-        }, msTimeout);
-    });
+async function verifyTx(txHash: string, msTimeout: number): Promise<Result<number>> {
+  return new Promise((res, err) => {
+    setTimeout(async () => {
+      const web3 = await getWeb3();
+      web3.eth
+        .getTransactionReceipt(txHash)
+        .then(receipt =>
+          res({
+            success: true,
+            result: receipt.blockNumber,
+          })
+        )
+        .catch((error: any) => err(Error(error.message)));
+    }, msTimeout);
+  });
 }
 
-export async function submitToBatcher(
-    subunit: BatchedSubunit
-): Promise<Result<number>> {
-    const errorFxn = buildEndpointErrorFxn("submitToBatcher");
-    const body = batchedToJsonString(subunit);
+export async function submitToBatcher(subunit: BatchedSubunit): Promise<Result<number>> {
+  const errorFxn = buildEndpointErrorFxn('submitToBatcher');
+  const body = batchedToJsonString(subunit);
 
-    let response: BatcherPostResponse;
-    let hash: string;
+  let response: BatcherPostResponse;
+  let hash: string;
 
-    try {
-        const result = await postToBatcher("submit_user_input", body);
-        if (!result.success) {
-            return errorFxn(
-                CatapultMiddlewareErrorCode.ERROR_POSTING_TO_BATCHER
-            );
-        }
-        // TODO: verify?
-        response = result.result as BatcherPostResponse;
-    } catch (err) {
-        return errorFxn(CatapultMiddlewareErrorCode.ERROR_POSTING_TO_BATCHER);
+  try {
+    const result = await postToBatcher('submit_user_input', body);
+    if (!result.success) {
+      return errorFxn(CatapultMiddlewareErrorCode.ERROR_POSTING_TO_BATCHER);
     }
+    // TODO: verify?
+    response = result.result as BatcherPostResponse;
+  } catch (err) {
+    return errorFxn(CatapultMiddlewareErrorCode.ERROR_POSTING_TO_BATCHER);
+  }
 
-    try {
-        if (!response.success) {
-            const msg = `Batcher rejected input "${body}" with response "${response.message}"`;
-            return errorFxn(
-                CatapultMiddlewareErrorCode.BATCHER_REJECTED_INPUT,
-                msg
-            );
-        }
-        hash = response.hash;
-    } catch (err) {
-        return errorFxn(
-            CatapultMiddlewareErrorCode.INVALID_RESPONSE_FROM_BATCHER,
-            err
-        );
+  try {
+    if (!response.success) {
+      const msg = `Batcher rejected input "${body}" with response "${response.message}"`;
+      return errorFxn(CatapultMiddlewareErrorCode.BATCHER_REJECTED_INPUT, msg);
     }
+    hash = response.hash;
+  } catch (err) {
+    return errorFxn(CatapultMiddlewareErrorCode.INVALID_RESPONSE_FROM_BATCHER, err);
+  }
 
-    try {
-        for (let i = 0; i < BATCHER_RETRIES; i++) {
-            await wait(BATCHER_WAIT_PERIOD);
-            //console.log(`[submitToBatcher] tracking data, attempt ${i + 1}`);
-            const result = await batcherEndpointCall(
-                `track_user_input?input_hash=${hash}`
-            );
-            if (!result.success) {
-                continue;
-            }
-            // TODO: verify?
-            const status: BatcherTrackResponse =
-                result.result as BatcherTrackResponse;
-            if (status.status === "rejected") {
-                const msg = `Batcher rejected input "${body}" with response "${status.message}"`;
-                return errorFxn(
-                    CatapultMiddlewareErrorCode.BATCHER_REJECTED_INPUT,
-                    msg
-                );
-            } else if (status.status === "posted") {
-                return {
-                    success: true,
-                    result: status.block_height,
-                };
-            }
-        }
-        return errorFxn(
-            CatapultMiddlewareErrorCode.FAILURE_VERIFYING_BATCHER_ACCEPTANCE
-        );
-    } catch (err) {
-        return errorFxn(
-            CatapultMiddlewareErrorCode.FAILURE_VERIFYING_BATCHER_ACCEPTANCE,
-            err
-        );
+  try {
+    for (let i = 0; i < BATCHER_RETRIES; i++) {
+      await wait(BATCHER_WAIT_PERIOD);
+      //console.log(`[submitToBatcher] tracking data, attempt ${i + 1}`);
+      const result = await batcherEndpointCall(`track_user_input?input_hash=${hash}`);
+      if (!result.success) {
+        continue;
+      }
+      // TODO: verify?
+      const status: BatcherTrackResponse = result.result as BatcherTrackResponse;
+      if (status.status === 'rejected') {
+        const msg = `Batcher rejected input "${body}" with response "${status.message}"`;
+        return errorFxn(CatapultMiddlewareErrorCode.BATCHER_REJECTED_INPUT, msg);
+      } else if (status.status === 'posted') {
+        return {
+          success: true,
+          result: status.block_height,
+        };
+      }
     }
+    return errorFxn(CatapultMiddlewareErrorCode.FAILURE_VERIFYING_BATCHER_ACCEPTANCE);
+  } catch (err) {
+    return errorFxn(CatapultMiddlewareErrorCode.FAILURE_VERIFYING_BATCHER_ACCEPTANCE, err);
+  }
 }
--- middleware/src/state.ts
+++ middleware/src/state.ts
@@ -1,39 +1,39 @@
 import {
-    BACKEND_URI,
-    BATCHER_URI,
-    CardanoAddress,
-    CHAIN_ID,
-    CHAIN_NAME,
-    CHAIN_URI,
-    ContractAddress,
-    DEFAULT_FEE,
-    DEPLOYMENT,
-    EthAddress,
-    INDEXER_URI,
-    STORAGE_ADDRESS,
-    URI,
-} from "@tower-defense/utils";
-import type { ETHAddress, Web3 } from "paima-engine/paima-utils";
-import { initWeb3 } from "paima-engine/paima-utils";
-import { CatapultMiddlewareErrorCode, errorMessageFxn } from "./errors";
+  BACKEND_URI,
+  BATCHER_URI,
+  CardanoAddress,
+  CHAIN_ID,
+  CHAIN_NAME,
+  CHAIN_URI,
+  ContractAddress,
+  DEFAULT_FEE,
+  DEPLOYMENT,
+  EthAddress,
+  INDEXER_URI,
+  STORAGE_ADDRESS,
+  URI,
+} from '@tower-defense/utils';
+import type { ETHAddress, Web3 } from 'paima-engine/paima-utils';
+import { initWeb3 } from 'paima-engine/paima-utils';
+import { CatapultMiddlewareErrorCode, errorMessageFxn } from './errors';
 import type {
-    CardanoApi,
-    Deployment,
-    MiddlewareConnectionDetails,
-    PostingInfo,
-    PostingModeString,
-} from "./types";
+  CardanoApi,
+  Deployment,
+  MiddlewareConnectionDetails,
+  PostingInfo,
+  PostingModeString,
+} from './types';
 
 export const enum PostingMode {
-    UNBATCHED,
-    BATCHED_ETH,
-    BATCHED_CARDANO,
+  UNBATCHED,
+  BATCHED_ETH,
+  BATCHED_CARDANO,
 }
 
 export const POSTING_MODE_NAMES: { [key: number]: PostingModeString } = {
-    [PostingMode.UNBATCHED]: "unbatched",
-    [PostingMode.BATCHED_ETH]: "batched-eth",
-    [PostingMode.BATCHED_CARDANO]: "batched-cardano",
+  [PostingMode.UNBATCHED]: 'unbatched',
+  [PostingMode.BATCHED_ETH]: 'batched-eth',
+  [PostingMode.BATCHED_CARDANO]: 'batched-cardano',
 };
 
 const backendUri: URI = BACKEND_URI;
@@ -46,12 +46,12 @@ const chainName: string = CHAIN_NAME;
 
 const storageAddress: ContractAddress = STORAGE_ADDRESS;
 
-const deployment: Deployment = DEPLOYMENT;
+const deployment = DEPLOYMENT as Deployment;
 
 let postingMode: PostingMode = PostingMode.UNBATCHED;
 
-let ethAddress: EthAddress = "";
-let cardanoAddress: CardanoAddress = "";
+let ethAddress: EthAddress = '';
+let cardanoAddress: CardanoAddress = '';
 let cardanoApi: CardanoApi = undefined;
 
 let web3: Web3 | undefined = undefined;
@@ -70,22 +70,19 @@ export const getStorageAddress = (): ContractAddress => storageAddress;
 export const getDeployment = (): Deployment => deployment;
 
 export const getPostingMode = (): PostingMode => postingMode;
-export const getPostingModeString = (): PostingModeString =>
-    POSTING_MODE_NAMES[postingMode];
+export const getPostingModeString = (): PostingModeString => POSTING_MODE_NAMES[postingMode];
 const setPostingMode = (newMode: PostingMode) => (postingMode = newMode);
 export const setUnbatchedMode = () => setPostingMode(PostingMode.UNBATCHED);
 export const setBatchedEthMode = () => setPostingMode(PostingMode.BATCHED_ETH);
-export const setBatchedCardanoMode = () =>
-    setPostingMode(PostingMode.BATCHED_CARDANO);
+export const setBatchedCardanoMode = () => setPostingMode(PostingMode.BATCHED_CARDANO);
 
 export const setEthAddress = (addr: ETHAddress) => (ethAddress = addr);
 export const getEthAddress = (): EthAddress => ethAddress;
-export const ethConnected = (): boolean => ethAddress !== "";
+export const ethConnected = (): boolean => ethAddress !== '';
 
-export const setCardanoAddress = (addr: CardanoAddress) =>
-    (cardanoAddress = addr);
+export const setCardanoAddress = (addr: CardanoAddress) => (cardanoAddress = addr);
 export const getCardanoAddress = (): CardanoAddress => cardanoAddress;
-export const cardanoConnected = (): boolean => cardanoAddress !== "";
+export const cardanoConnected = (): boolean => cardanoAddress !== '';
 
 export const setCardanoApi = (api: CardanoApi) => (cardanoApi = api);
 export const getCardanoApi = (): CardanoApi => cardanoApi;
@@ -94,34 +91,33 @@ export const setFee = (newFee: string) => (fee = newFee);
 export const getFee = (): string => fee;
 
 export const getWeb3 = async (): Promise<Web3> => {
-    if (typeof web3 === "undefined") {
-        web3 = await initWeb3(chainUri);
-    }
-    return web3;
+  if (typeof web3 === 'undefined') {
+    web3 = await initWeb3(chainUri);
+  }
+  return web3;
 };
 
 export const getActiveAddress = (): string => {
-    switch (postingMode) {
-        case PostingMode.UNBATCHED:
-        case PostingMode.BATCHED_ETH:
-            return ethAddress;
-        case PostingMode.BATCHED_CARDANO:
-            return cardanoAddress;
-        default:
-            const errorCode =
-                CatapultMiddlewareErrorCode.INTERNAL_INVALID_POSTING_MODE;
-            throw new Error(errorMessageFxn(errorCode));
-    }
+  switch (postingMode) {
+    case PostingMode.UNBATCHED:
+    case PostingMode.BATCHED_ETH:
+      return ethAddress;
+    case PostingMode.BATCHED_CARDANO:
+      return cardanoAddress;
+    default:
+      const errorCode = CatapultMiddlewareErrorCode.INTERNAL_INVALID_POSTING_MODE;
+      throw new Error(errorMessageFxn(errorCode));
+  }
 };
 
 export const getConnectionDetails = (): MiddlewareConnectionDetails => ({
-    storageAddress,
-    backendUri,
-    indexerUri,
-    batcherUri,
+  storageAddress,
+  backendUri,
+  indexerUri,
+  batcherUri,
 });
 
 export const getPostingInfo = (): PostingInfo => ({
-    address: getActiveAddress(),
-    postingModeString: getPostingModeString(),
+  address: getActiveAddress(),
+  postingModeString: getPostingModeString(),
 });
