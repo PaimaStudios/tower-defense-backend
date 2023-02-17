33e35b136d7ebd6b95935671ccc755b516b204ef
mirtyl-wacdec <ycl2@protonmail.com>
2023-02-17 17:47:36 +0700

    revived main backend function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import paimaFunnel from 'paima-engine/paima-funnel';
 import paimaRuntime from 'paima-engine/paima-runtime';
 import gameSM from './sm.js';
-// import registerEndpoints from '@tower-defense/api';
+import registerEndpoints from '@tower-defense/api';
 import {
   STOP_BLOCKHEIGHT,
   STORAGE_ADDRESS,
@@ -13,13 +13,13 @@ import { setPool } from '@tower-defense/db';
 const POLLING_RATE = 1;
 
 async function main() {
-  // console.log(STORAGE_ADDRESS);
-  // const chainFunnel = await paimaFunnel.initialize(CHAIN_URI, STORAGE_ADDRESS);
-  // setPool(gameSM.getReadonlyDbConn());
-  // const engine = paimaRuntime.initialize(chainFunnel, gameSM, gameBackendVersion);
-  // engine.setPollingRate(POLLING_RATE);
-  // engine.addEndpoints(registerEndpoints);
-  // engine.run(STOP_BLOCKHEIGHT, SERVER_ONLY_MODE);
+  console.log(STORAGE_ADDRESS, "storage address");
+  const chainFunnel = await paimaFunnel.initialize(CHAIN_URI, STORAGE_ADDRESS);
+  setPool(gameSM.getReadonlyDbConn());
+  const engine = paimaRuntime.initialize(chainFunnel, gameSM, gameBackendVersion);
+  engine.setPollingRate(POLLING_RATE);
+  engine.addEndpoints(registerEndpoints);
+  engine.run(STOP_BLOCKHEIGHT, SERVER_ONLY_MODE);
 }
 
 main();
