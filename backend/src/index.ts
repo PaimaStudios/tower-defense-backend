fc60f97a5263356fbec2d836bcaed11313da88aa
Lukas Miskovsky <miskovskylukas@gmail.com>
2023-03-29 13:10:33 +0200

    Update env files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,11 @@ import gameSM from './sm.js';
 import registerEndpoints from '@tower-defense/api';
 import { gameBackendVersion, GameENV } from '@tower-defense/utils';
 import { setPool } from '@tower-defense/db';
+
 const POLLING_RATE = 1;
 
 async function main() {
-  console.log(GameENV.CONTRACT_ADDRESS, 'storage address');
+  console.log(GameENV.CONTRACT_ADDRESS);
   const chainFunnel = await paimaFunnel.initialize(GameENV.CHAIN_URI, GameENV.CONTRACT_ADDRESS);
   setPool(gameSM.getReadonlyDbConn());
   const engine = paimaRuntime.initialize(chainFunnel, gameSM, gameBackendVersion);
