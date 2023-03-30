df72848339326506030cff357f8a80d19f8d73ed
Lukas Miskovsky <miskovskylukas@gmail.com>
2023-03-30 10:12:59 +0200

    Fix empty random lobby

--- api/src/controllers/openLobbies.ts
+++ api/src/controllers/openLobbies.ts
@@ -24,34 +24,21 @@ export class openLobbiesController extends Controller {
     const valCount = psqlNum.decode(count || 10); // pass 10 if undefined (or 0)
     // io-ts output typecheck. isLeft() is invalid, isRight() is valid
     // we'll reuse TSOA's error handling logic to throw an error
-    if (isLeft(valPage))
-      throw new ValidateError(
-        {
-          page: {
-            message: 'invalid number',
-          },
-        },
-        ''
-      );
-    else if (isLeft(valCount))
-      throw new ValidateError(
-        {
-          count: {
-            message: 'invalid number',
-          },
-        },
-        ''
-      );
-    else {
-      wallet = wallet.toLowerCase();
-      const p = valPage.right;
-      const c = valCount.right;
-      const offset = (p - 1) * c;
-      const lobbies = await getPaginatedOpenLobbies.run(
-        { count: `${c}`, page: `${offset}`, wallet },
-        pool
-      );
-      return { lobbies };
+    if (isLeft(valPage)) {
+      throw new ValidateError({ page: { message: 'invalid number' } }, '');
     }
+    if (isLeft(valCount)) {
+      throw new ValidateError({ count: { message: 'invalid number' } }, '');
+    }
+
+    wallet = wallet.toLowerCase();
+    const p = valPage.right;
+    const c = valCount.right;
+    const offset = (p - 1) * c;
+    const lobbies = await getPaginatedOpenLobbies.run(
+      { count: `${c}`, page: `${offset}`, wallet },
+      pool
+    );
+    return { lobbies };
   }
 }
