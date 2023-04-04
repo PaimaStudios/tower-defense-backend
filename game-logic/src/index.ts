26f83fdb4afa44a960b8f1115653b25e3a20eac7
Lukas Miskovsky <miskovskylukas@gmail.com>
2023-04-04 09:30:14 +0200

    Fix wrong config helper export

--- game-logic/src/index.ts
+++ game-logic/src/index.ts
@@ -1,6 +1,6 @@
 import processTick from './processTick';
 export default processTick;
 export { getMap } from './map-processor';
-export { parseConfig } from './config';
+export { parseConfig, conciseFactionMap } from './config';
 export { validateMoves } from './validation';
 export { generateRandomMoves } from './ai';
