73f458c0b6d00e2b8c4e78483d4202a175fe4e60
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2023-05-16 08:10:55 +0200

    update

--- test-frontend/src/Simulation.tsx
+++ test-frontend/src/Simulation.tsx
@@ -1,8 +1,54 @@
-import type { MatchState } from '@tower-defense/utils';
+import type {
+  AttackerStructure,
+  CryptConfig,
+  DefenderStructure,
+  MatchConfig,
+  MatchState,
+  TowerConfig,
+} from '@tower-defense/utils';
 import React, { useState } from 'react';
 
-const GameApiResponseDisplay: React.FC<{ response: MatchState }> = ({ response }) => {
+const GameApiResponseDisplay: React.FC<{ response: MatchState; config: MatchConfig }> = ({
+  response,
+  config,
+}) => {
   const { crypts, towers } = response.actors;
+
+  const calculateCooldownTime = (actor: DefenderStructure | AttackerStructure): number => {
+    const structureConfig = config[actor.structure][actor.upgrades];
+    let cooldown: number;
+    if ('attackCooldown' in structureConfig) {
+      cooldown = structureConfig.attackCooldown;
+    } else {
+      cooldown = structureConfig.cooldown;
+    }
+    const cooldownTime = (1 / config.baseSpeed) * cooldown;
+    return cooldownTime;
+  };
+
+  const battalionHP = (actor: DefenderStructure | AttackerStructure): number => {
+    const structureConfig: CryptConfig | TowerConfig = config[actor.structure][actor.upgrades];
+    let hp: number;
+    if ('unitHealth' in structureConfig) {
+      return structureConfig.unitHealth * structureConfig.spawnCapacity;
+    } else {
+      return 0;
+    }
+  };
+
+  const damagePerSec = (actor: DefenderStructure | AttackerStructure, cooldown: number): number => {
+    const structureConfig: CryptConfig | TowerConfig = config[actor.structure][actor.upgrades];
+    let damage: number;
+    if ('attackDamage' in structureConfig) {
+      damage = actor.structure === 'macawCrypt' ? structureConfig.attackDamage : 0;
+    } else {
+      damage = structureConfig.damage;
+    }
+    const quantity = 'spawnCapacity' in structureConfig ? structureConfig.spawnCapacity : 1;
+    const totalDamage = (damage * quantity) / cooldown;
+    return totalDamage;
+  };
+
   return (
     <div style={{ display: 'flex', justifyContent: 'space-around' }}>
       <div>
@@ -12,28 +58,39 @@ const GameApiResponseDisplay: React.FC<{ response: MatchState }> = ({ response }
       </div>
       <div>
         <h2>Towers</h2>
-        {Object.values(towers).map(tower => (
-          <div key={tower.id}>
-            <p>
-              Structure: <b>{tower.structure}</b>
-            </p>
-            <p>Health: {tower.health}</p>
-            <p>Coordinates: {tower.coordinates}</p>
-            <p>Upgrades: {tower.upgrades}</p>
-          </div>
-        ))}
+        {Object.values(towers).map(tower => {
+          const cooldownTime = calculateCooldownTime(tower);
+          return (
+            <div key={tower.id}>
+              <p>
+                Structure: <b>{tower.structure}</b>
+              </p>
+              <p>Health: {tower.health}</p>
+              <p>Coordinates: {tower.coordinates}</p>
+              <p>Upgrades: {tower.upgrades}</p>
+              <p>Power per sec: {damagePerSec(tower, cooldownTime).toFixed(2)}</p>
+            </div>
+          );
+        })}
       </div>
       <div>
         <h2>Crypts</h2>
-        {Object.values(crypts).map(crypt => (
-          <div key={crypt.id}>
-            <p>
-              Structure: <b>{crypt.structure}</b>
-            </p>
-            <p>Coordinates: {crypt.coordinates}</p>
-            <p>Upgrades: {crypt.upgrades}</p>
-          </div>
-        ))}
+        {Object.values(crypts).map(crypt => {
+          const cooldownTime = calculateCooldownTime(crypt);
+
+          return (
+            <div key={crypt.id}>
+              <p>
+                Structure: <b>{crypt.structure}</b>
+              </p>
+              <p>Coordinates: {crypt.coordinates}</p>
+              <p>Units: {crypt.spawned.length}</p>
+              <p>Upgrades: {crypt.upgrades}</p>
+              <p>Battalion HP: {battalionHP(crypt)}</p>
+              <p>Power per sec: {damagePerSec(crypt, cooldownTime).toFixed(2)}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
@@ -41,9 +98,10 @@ const GameApiResponseDisplay: React.FC<{ response: MatchState }> = ({ response }
 
 interface IGameApiDisplayProps {
   data: MatchState[];
+  config: MatchConfig;
 }
 
-const GameApiDisplay: React.FC<IGameApiDisplayProps> = ({ data }) => {
+const GameApiDisplay: React.FC<IGameApiDisplayProps> = ({ data, config }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const sliderSize = 3;
 
@@ -71,7 +129,7 @@ const GameApiDisplay: React.FC<IGameApiDisplayProps> = ({ data }) => {
         {data.slice(currentIndex, currentIndex + sliderSize).map((response, index) => (
           <div key={index}>
             <div className="separation-line"></div>
-            <GameApiResponseDisplay response={response} />
+            <GameApiResponseDisplay response={response} config={config} />
           </div>
         ))}
       </div>
