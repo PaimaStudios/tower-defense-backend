360b770a9a6470feb70486c1215a50e2da7ee9fb
Lukas Miskovsky <miskovskylukas@gmail.com>
2023-06-15 15:06:46 +0200

    Add db stats script

--- db/stats.js
+++ db/stats.js
@@ -11,8 +11,7 @@ const creds = {
 const WINNERS_LIMIT = 120;
 const START_BLOCKHEIGHT = 11883010;
 
-const selectWinners = async () => {
-  const pool = new pg.Pool(creds);
+const selectWinners = async pool => {
   const winners = await pool.query(`
   SELECT 
     final_match_state.lobby_id,
@@ -38,4 +37,31 @@ const selectWinners = async () => {
   console.log(firstWinners);
 };
 
-selectWinners();
+const selectGamesPlayed = async pool => {
+  const totalGames = await pool.query(`SELECT COUNT(*) FROM final_match_state`);
+  console.log('Total games played: ');
+  console.log(totalGames.rows);
+};
+
+const selectUniquePlayers = async pool => {
+  const players = await pool.query(`
+    SELECT 
+      COUNT(DISTINCT user_address) FILTER (WHERE user_address LIKE '0x%') AS evm,
+      COUNT(DISTINCT user_address) FILTER (WHERE user_address LIKE 'addr%') AS cardano,
+      COUNT(DISTINCT user_address) - COUNT(DISTINCT user_address) FILTER (WHERE user_address LIKE 'addr%') - COUNT(DISTINCT user_address) FILTER (WHERE user_address LIKE '0x%') as polkadot
+    FROM historical_game_inputs
+`);
+  console.log('Unique players wallet distribution: ');
+  console.log(players.rows);
+};
+
+const run = async () => {
+  const pool = new pg.Pool(creds);
+  await selectWinners(pool);
+  await selectGamesPlayed(pool);
+  await selectUniquePlayers(pool);
+
+  process.exit(0);
+};
+
+run();
