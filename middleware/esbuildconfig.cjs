0aff92b36cbb8f30e4bc7da14deb944709611f12
Sebastien Guillemot <sebastiengllmt@gmail.com>
2023-08-17 03:10:16 +0900

    Update middleware parsing logic

--- middleware/esbuildconfig.cjs
+++ middleware/esbuildconfig.cjs
@@ -1,6 +1,5 @@
-const g = require('@esbuild-plugins/node-globals-polyfill');
-const m = require('@esbuild-plugins/node-modules-polyfill');
-const modules = m.NodeModulesPolyfillPlugin();
+const { polyfillNode } = require('esbuild-plugin-polyfill-node');
+const fs = require('fs');
 
 const define = { global: 'window' };
 // To replace process.env calls in middleware with variable values during build time
@@ -8,10 +7,20 @@ for (const variable in process.env) {
   define[`process.env.${variable}`] = JSON.stringify(process.env[variable]);
 }
 
-const global = g.NodeGlobalsPolyfillPlugin({
+if (process.env.SECURITY_NAMESPACE) {
+  const namespace = process.env.SECURITY_NAMESPACE;
+  if (namespace.endsWith('.yml') || namespace.endsWith('.yaml')) {
+    const fileContent = fs.readFileSync(`../${namespace}`, 'utf-8');
+    define[`process.env.SECURITY_NAMESPACE_ROUTER`] = JSON.stringify(fileContent);
+  }
+}
+
+const nodePolyfills = polyfillNode({
   process: true,
   buffer: true,
-  define: { 'process.env.var': '"hello"' }, // inject will override define, to keep env vars you must also pass define here https://github.com/evanw/esbuild/issues/660
+  polyfills: {
+    'fs/promises': true,
+  }, // inject will override define, to keep env vars you must also pass define here https://github.com/evanw/esbuild/issues/660
 });
 const esbuild = require('esbuild');
 const config = {
@@ -21,7 +30,7 @@ const config = {
   format: 'esm',
   define,
   outfile: 'packaged/middleware.js',
-  plugins: [global, modules],
+  plugins: [nodePolyfills],
   external: ['pg-native'],
 };
 
