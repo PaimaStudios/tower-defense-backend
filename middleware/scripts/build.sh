ed1020063047a98a67e567a656d5f34e3ef27751
Tad Hardesty <tad@dcspark.io>
2024-09-13 15:09:01 -0700

    Remove fsa.ts manipulation from middleware build

--- middleware/scripts/build.sh
+++ middleware/scripts/build.sh
@@ -1,22 +1,9 @@
-# echo "Installing dependencies"
-# npm i
-
-cd ..
-npm run build
-cd middleware
-
-
-sh scripts/set_fs_removal.sh
-echo "FS dependency removed"
+#!/bin/sh
 
 ENVIRONMENT=${NODE_ENV:-development}
 echo "Packaging Middleware for $ENVIRONMENT"
 DOTENV_CONFIG_PATH="../.env.$ENVIRONMENT" node --require dotenv/config ./esbuildconfig.cjs
 echo "Finished Packaging"
-
-sh scripts/undo_fs_removal.sh
-echo "FS dependency re-set"
-
 
 echo "Vanilla Middleware (With Exports) Prepared In: packaged/middleware.js"
 head -n $(( $(grep -n '^export {' packaged/middleware.js | head -1 | cut -d: -f1) - 1 )) packaged/middleware.js > packaged/paimaMiddleware.js
