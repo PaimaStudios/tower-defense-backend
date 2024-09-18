ccd4792dded070f0c8c34ec36584027d7783bd23
Tad Hardesty <tad@dcspark.io>
2024-09-18 14:55:10 -0700

    Remove copypaste of paima-batcher

--- paima-batcher/utils/src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const enum AddressType {
-  UNKNOWN = 0,
-  EVM = 1,
-  CARDANO = 2,
-  POLKADOT = 3,
-  ALGORAND = 4,
-}
--- paima-batcher/utils/src/version.ts
+++
@@ -1,7 +0,0 @@
-type VersionString = `${number}.${number}.${number}`;
-
-const VERSION_MAJOR = 0;
-const VERSION_MINOR = 0;
-const VERSION_PATCH = 1;
-
-export const VERSION_STRING: VersionString = `${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}`;
