4f6cb678744300d9e4a43a386ab94caae22709fc
Tad Hardesty <tad@dcspark.io>
2024-10-22 14:01:19 -0700

    Let backend proxy & cache NFT images from IPFS

--- api/src/controllers/accountNfts.ts
+++ api/src/controllers/accountNfts.ts
@@ -1,45 +1,10 @@
+import { ENV } from '@paima/utils';
 import { getOwnedNfts } from '@paima/utils-backend';
 import { requirePool } from '@tower-defense/db';
 import { Controller, Get, Query, Route } from 'tsoa';
-import { Abi, createPublicClient, getContract, http } from 'viem';
-import annotatedMintNftAbi from '@paima/evm-contracts/abi/AnnotatedMintNft.json' with { type: 'json' };
-
-const publicClient = createPublicClient({
-  // TODO: un-hardcode Hardhat here
-  chain: {
-    // config chainId
-    id: 31_337,
-    // probably the config key would work here
-    name: 'Hardhat',
-    nativeCurrency: {
-      // config chainCurrencyDecimals
-      decimals: 18,
-      // config chainCurrencyName
-      name: 'Ether',
-      // config chainCurrencySymbol
-      symbol: 'ETH',
-    },
-    rpcUrls: {
-      // config chainUri
-      default: { http: ['http://127.0.0.1:8545'] },
-    },
-  },
-  transport: http(),
-});
 
 // TODO: un-hardcode contract address.
 const contract = '0xe7f1725e7734ce288f8367e1bb143e90bb3f0512';
-
-const trainers = getContract({
-  abi: annotatedMintNftAbi as unknown as Abi,
-  address: contract,
-  client: publicClient,
-});
-
-function ipfsToHttp(ipfs: string): string {
-  // nftstorage.link because it's what we use in Tarochi
-  return ipfs.replace(/^ipfs:\/\//, 'https://nftstorage.link/ipfs/');
-}
 
 interface AccountNftsResult {
   metadata: {
@@ -67,27 +32,25 @@ export class AccountNftsController extends Controller {
     console.log('account-nfts', account, page, size);
 
     const pool = requirePool();
-    const tokenIds = await getOwnedNfts(pool, 'EVM Genesis Trainers', account);
-
-    const tokenId = tokenIds[page];
-
-    const metadataUri = (await trainers.read.tokenURI([tokenId])) as string;
-    const metadata = await (await fetch(ipfsToHttp(metadataUri))).json();
-
-    const item = {
-      metadata: {
-        name: metadata.name,
-        image: ipfsToHttp(metadata.image),
-      },
-      contract,
-      tokenId: Number(tokenId),
-    };
+    let tokenIds = await getOwnedNfts(pool, 'EVM Genesis Trainers', account);
+    const totalItems = tokenIds.length, pages = Math.ceil(totalItems / size);
+    tokenIds = tokenIds.slice(page * size, (page + 1) * size);
 
     return {
       response: {
-        pages: tokenIds.length,
-        totalItems: tokenIds.length,
-        result: [item],
+        pages,
+        totalItems,
+        result: tokenIds.map(id => ({
+          // Save on having to hit IPFS here by hardcoding stuff.
+          metadata: {
+            // The frontend doesn't actually use this.
+            name: `Genesis Trainer #${id}`,
+            // See TrainerImageController.
+            image: `${ENV.BACKEND_URI}/trainer-image/${id}.png`,
+          },
+          contract,
+          tokenId: Number(id),
+        })),
       },
     };
   }
