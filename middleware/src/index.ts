f3aded2c57cb6b8f7d4ca41924a31907c0afc7ed
Tad Hardesty <tad@dcspark.io>
2024-10-25 17:47:37 -0700

    Post wallet delegation message to batcher, process it

--- middleware/src/index.ts
+++ middleware/src/index.ts
@@ -1,4 +1,11 @@
-import { LoginInfo, paimaEndpoints, Wallet, WalletConnectHelper } from '@paima/mw-core';
+import {
+  LoginInfo,
+  paimaEndpoints,
+  PostDataResponse,
+  Wallet,
+  walletConnect,
+  WalletConnectHelper,
+} from '@paima/mw-core';
 import {
   initMiddlewareCore,
   userWalletLoginWithoutChecks,
@@ -29,6 +36,25 @@ const localWalletReady = localWallet.loadOrCreate({
     password: 'paima-tower-defense-wallet-password',
   },
 });
+// Immediately start userWalletLogin to speed things up a smidge.
+const localWalletResult = (async () => {
+  await localWalletReady;
+
+  let loginInfo: LoginInfo = {
+    mode: WalletMode.EvmEthers,
+    preferBatchedMode: true,
+    connection: {
+      metadata: {
+        name: 'thirdweb.localwallet',
+        displayName: 'Local Wallet',
+      },
+      // type pun from ethers@5 to ethers@6 Signer
+      api: (await localWallet.getSigner()) as any,
+    },
+  };
+
+  return await paimaEndpoints.userWalletLogin(loginInfo);
+})();
 
 const endpoints = {
   ...paimaEndpoints,
@@ -36,45 +62,35 @@ const endpoints = {
   ...writeEndpoints,
 
   async userWalletLogin(name: string): Promise<Result<Wallet>> {
-    /*
-    let loginInfo: LoginInfo = {
-      mode: WalletMode.EvmInjected,
-      preferBatchedMode: false,
-      preference: {
-        name,
-      },
-      checkChainId: false,
-    };
-    */
-
-    await localWalletReady;
-
-    let loginInfo: LoginInfo = {
-      mode: WalletMode.EvmEthers,
-      preferBatchedMode: true,
-      connection: {
-        metadata: {
-          name: 'thirdweb.localwallet',
-          displayName: 'Local Wallet',
-        },
-        // type pun from ethers@5 to ethers@6 Signer
-        api: (await localWallet.getSigner()) as any,
-      },
-    };
-
-    return await paimaEndpoints.userWalletLogin(loginInfo);
+    return localWalletResult;
   },
 
-  async externalWalletConnect(name: string): Promise<object> {
+  async externalWalletConnect(name: string): Promise<Result<PostDataResponse>> {
     await localWalletReady;
 
-    await new WalletConnectHelper().connectExternalWalletAndSign(
+    const localWalletAddress = await localWallet.getAddress();
+    const externalSignsLocal = await new WalletConnectHelper().connectExternalWalletAndSign(
       AddressType.EVM,
       name,
-      await localWallet.getAddress()
+      localWalletAddress
     );
 
-    return {};
+    const toSign = new WalletConnectHelper().buildMessageToSign(externalSignsLocal.walletAddress);
+
+    const localSignsExternal = {
+      message: toSign,
+      walletAddress: localWalletAddress,
+      signedMessage: await localWallet.signMessage(toSign),
+    };
+
+    const posted = await walletConnect(
+      externalSignsLocal.walletAddress,
+      null, // 'to' is implied server-side by the submitting wallet address
+      externalSignsLocal.signedMessage,
+      localSignsExternal.signedMessage
+    );
+
+    return posted;
   },
 };
 
