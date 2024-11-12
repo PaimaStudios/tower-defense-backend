5a64e1978f94f0a897beb57c4b51b99d975949c1
Tad Hardesty <tad@dcspark.io>
2024-11-12 09:46:10 -0800

    Add partial leaderboard API

--- api/src/controllers/leaderboard.ts
+++ api/src/controllers/leaderboard.ts
@@ -1,5 +1,6 @@
-import { requirePool } from '@tower-defense/db';
-import { Controller, Get, Query, Route } from 'tsoa';
+import { getNftLeaderboards, requirePool } from '@tower-defense/db';
+import { Controller, Get, Query, Request, Route } from 'tsoa';
+import { Request as ExpressRequest } from 'express';
 
 interface LeaderboardEntryType {
   token_id: number;
@@ -21,13 +22,31 @@ interface LeaderboardEntryProps extends LeaderboardEntryType {
   wrapperClassname?: string;
 }
 
-@Route('leaderboard')
-export class LeaderboardController extends Controller {
+@Route('leaderboards')
+export class LeaderboardsController extends Controller {
   @Get()
-  public async get(@Query() frequency: string, @Query() previous: boolean): Promise<LeaderboardEntryProps[]> {
+  public async get(@Request() request: ExpressRequest, @Query() frequency: string, @Query() previous: boolean): Promise<LeaderboardEntryProps[]> {
     console.log('/leaderboard', frequency, previous);
     const pool = requirePool();
 
-    return [];
+    const nfts = await getNftLeaderboards.run(undefined, pool);
+    return nfts.map((nft, index) => {
+      return {
+        token_id: Number(nft.token_id),
+        nft_contract: 'TODO',
+        wins: nft.wins,
+        draws: 0,
+        losses: nft.losses,
+        total_games: nft.wins + nft.losses,
+        score: nft.wins * 10,  // TODO
+        current_streak: 17,  // TODO
+        longest_streak: 21,  // TODO
+
+        position: index + 1,  // TODO: ties
+        avatar_url: `/trainer-image/${nft.token_id}.png`,
+        name: 'MY NAME',
+        wallet_address: nft.nft_owner,
+      }
+    });
   }
 }
