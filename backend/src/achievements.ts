9224b56970a1cc4a3c4d5faea2cca674a59f351d
Tad Hardesty <tad@dcspark.io>
2024-11-19 10:39:41 -0800

    Stub out further achievement descriptions

--- backend/src/achievements.ts
+++ backend/src/achievements.ts
@@ -1,7 +1,14 @@
 import { AchievementMetadata } from "@paima/utils-backend";
 
 export enum AchievementNames {
-  WIN_RANKED_WITH_NFT = 'win_ranked_with_nft',
+  // Easy free
+  WIN_FIRST_MATCH = 'win_first_match',
+  // Medium free
+  DEFEND_WITH_HALF_HP = 'defend_with_half_hp',
+  // Medium paid
+  WIN_RANKED = 'win_ranked',
+  // Hard paid
+  WIN_EVERY_POSITION = 'win_every_position',
 }
 
 export const metadata: AchievementMetadata = {
@@ -10,11 +17,33 @@ export const metadata: AchievementMetadata = {
     name: 'Wrath of the Jungle: Tower Defense',
   },
   list: [
+    // Easy free
     {
-      name: AchievementNames.WIN_RANKED_WITH_NFT,
-      displayName: 'A Winner Is You',
+      name: AchievementNames.WIN_FIRST_MATCH,
       isActive: true,
-      description: 'Win a ranked match with an NFT'
+      displayName: 'Welcome to the Jungle',
+      description: 'Win your first match.',
+    },
+    // Medium free
+    {
+      name: AchievementNames.DEFEND_WITH_HALF_HP,
+      isActive: true,
+      displayName: 'The Best Defense',
+      description: 'Win as defender with at least half your health remaining.',
+    },
+    // Medium paid
+    {
+      name: AchievementNames.WIN_RANKED,
+      isActive: true,
+      displayName: 'Rope Ladder Climber',
+      description: 'Win your first ranked match.'
+    },
+    // Hard paid
+    {
+      name: AchievementNames.WIN_EVERY_POSITION,
+      isActive: true,
+      displayName: 'Jungle World Tour',
+      description: 'Win a ranked match as each side on each map.',
     }
   ]
 };
